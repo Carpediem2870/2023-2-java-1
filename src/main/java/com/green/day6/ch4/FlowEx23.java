5bc52de3c842d7042a7156c9fb28ae0b226b2067
carpediem <luvrel@naver.com>
2023-10-04 16:22:55 +0900

    4시22분수업

---
+++ src/main/java/com/green/day6/ch4/FlowEx23.java
@@ -0,0 +1,16 @@
+package com.green.day6.ch4;
+
+public class FlowEx23 {
+    public static void main(String[] args) {
+        int i = 5;
+//        4 - I can do it.
+//        3 - I can do it.
+//        2 - I can do it.
+//        1 - I can do it.
+//        0 - I can do it.
+        while(i !=0) {
+            i--;
+            System.out.printf("%d - I can do it.%n", i);
+        }
+    }
+}
--- src/main/java/com/green/day6/ch4/While.java
+++ src/main/java/com/green/day6/ch4/While.java
@@ -2,6 +2,17 @@ package com.green.day6.ch4;
 
 public class While {
     public static void main(String[] args) {
-        //while은 반복문 입니다. ggggggg 원본수정
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("for- " + i);
+        }
+
+        System.out.println("=================");
+
+        int i=0;
+        while(i<10) {
+            System.out.println("while- " + i);  // 아래 i++을 System.out.println("while- " + i++); 뒤로 옮겨도됨
+            i++;
+        }
     }
 }
